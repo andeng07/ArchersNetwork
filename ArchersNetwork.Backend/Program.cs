d533ac984413607fec2fbfb37652a8fdba1617be
Andrei Sumilang <sumilangandreijohn@gmail.com>
2025-09-24 12:44:51 +0800

    feat: register mongo client connection as a singleton service

--- ArchersNetwork.Backend/Program.cs
+++ ArchersNetwork.Backend/Program.cs
@@ -1,10 +1,17 @@
 using ArchersNetwork.Backend.Core.Endpoint;
 using ArchersNetwork.Backend.Features.Video;
+using MongoDB.Driver;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
 builder.Services.AddEndpoints();
+
+builder.Services.AddSingleton<IMongoClient>(provider =>
+{
+    var config = provider.GetRequiredService<IConfiguration>();
+    return new MongoClient(config["MongoDB:ConnectionString"]);
+});
 
 builder.Services.AddScoped<VideoService>();
 
