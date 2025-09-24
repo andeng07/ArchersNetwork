81c3f7868a92359074a5ca4568b0a49d4e963fb5
Andrei Sumilang <sumilangandreijohn@gmail.com>
2025-09-24 09:57:10 +0800

    refactor: change video id type from int to guid

--- ArchersNetwork.Backend/Features/Video/Model/Video.cs
+++ ArchersNetwork.Backend/Features/Video/Model/Video.cs
@@ -7,11 +7,11 @@ namespace ArchersNetwork.Backend.Features.Video.Model;
 /// </summary>
 public class Video
 {
-    public int Id { get; set; }
-    public string Title { get; set; }
-    public DateTime Date { get; set; }
-    public string Description { get; set; }
-    public string Link { get; set; }
-    public string Channel { get; set; }
-    public string Series { get; set; }
+    public required Guid Id { get; init; }
+    public required string Title { get; set; }
+    public required DateTime Date { get; set; }
+    public required string Description { get; set; }
+    public required string Link { get; set; }
+    public required string Channel { get; set; }
+    public required string Series { get; set; }
 }
