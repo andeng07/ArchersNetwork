a99ab42a44c558009fb47a02fab1d541e4cc1a64
Andrei Sumilang <sumilangandreijohn@gmail.com>
2025-09-24 11:25:55 +0800

    feat: map endpoints and register video service

--- ArchersNetwork.Backend/Program.cs
+++ ArchersNetwork.Backend/Program.cs
@@ -1,6 +1,12 @@
+using ArchersNetwork.Backend.Core.Endpoint;
+using ArchersNetwork.Backend.Features.Video;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
+builder.Services.AddEndpoints();
+
+builder.Services.AddScoped<VideoService>();
 
 var app = builder.Build();
 
@@ -9,4 +15,10 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
+var videoEndpointGroup = app.MapGroup("/videos");
+
+app.MapEndpoints(videoEndpointGroup);
+
 app.UseHttpsRedirection();
+
+app.Run();
